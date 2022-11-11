d0e2a338a707cb1365c3cd5bbd80b5d3353a605d
Amani Chennu <112126839+amanichennu201@users.noreply.github.com>
2022-11-11 11:48:56 -0600

    added changes

--- controllers/Bag.js
+++ controllers/Bag.js
@@ -11,9 +11,16 @@ exports.Bag_list = async function (req, res) {
     }
 };
 // for a specific Bag.
-exports.Bag_detail = function (req, res) {
-    res.send('NOT IMPLEMENTED: Bag detail: ' + req.params.id);
-};
+exports.Bag_detail = async function(req, res) { 
+    console.log("detail"  + req.params.id) 
+    try { 
+        result = await Bag.findById( req.params.id) 
+        res.send(result) 
+    } catch (error) { 
+        res.status(500) 
+        res.send(`{"error": document for id ${req.params.id} not found`); 
+    } 
+}; 
 // Handle Bag create on POST.
 exports.Bag_create_post = async function (req, res) {
     console.log(req.body)
