18d70b808aaeff01e47b08368e95120bde0af715
Amani Chennu <112126839+amanichennu201@users.noreply.github.com>
2022-11-13 21:12:29 -0600

    2

--- controllers/Bag.js
+++ controllers/Bag.js
@@ -46,9 +46,25 @@ exports.Bag_delete = function (req, res) {
     res.send('NOT IMPLEMENTED: Bag delete DELETE ' + req.params.id);
 };
 // Handle Bag update form on PUT.
-exports.Bag_update_put = function (req, res) {
-    res.send('NOT IMPLEMENTED: Bag update PUT' + req.params.id);
-};
+exports.Bag_update_put = async function(req, res) { 
+    console.log(`update on id ${req.params.id} with body 
+${JSON.stringify(req.body)}`) 
+    try { 
+        let toUpdate = await Bag.findById( req.params.id) 
+        // Do updates of properties 
+        if(req.body.Bag_type)  
+               toUpdate.Bag_type = req.body.Bag_type; 
+        if(req.body.Bag_cost) toUpdate.Bag_cost = req.body.Bag_cost; 
+        if(req.body.Bag_Name) toUpdate.Bag_Name = req.body.Bag_Name; 
+        let result = await toUpdate.save(); 
+        console.log("Sucess " + result) 
+        res.send(result) 
+    } catch (err) { 
+        res.status(500) 
+        res.send(`{"error": ${err}: Update for id ${req.params.id} 
+failed`); 
+    } 
+}; 
 
 //VIEWS
 //Handle a show all view
