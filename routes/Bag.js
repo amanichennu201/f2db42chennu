9a7fd5e3dc49d763436ccda92fcebed4a30aed4c
Amani Chennu <112126839+amanichennu201@users.noreply.github.com>
2022-11-30 23:22:27 -0600

    added some changes

--- routes/Bag.js
+++ routes/Bag.js
@@ -1,17 +1,23 @@
 var express = require('express');
 const Bag_controlers = require('../controllers/Bag');
 var router = express.Router();
-
+const secured = (req, res, next) => {
+    if (req.user){
+    return next();
+    }
+    req.session.returnTo = req.originalUrl;
+    res.redirect("/login");
+    }
 /* GET home page. */
 router.get('/', Bag_controlers.Bag_view_all_Page);
 /* GET detail bag page */
 router.get('/detail', Bag_controlers.Bag_view_one_Page);
 /* GET create bag page */
-router.get('/create', Bag_controlers.Bag_create_Page);
+router.get('/create',  secured, Bag_controlers.Bag_create_Page);
 /* GET create update page */
-router.get('/update', Bag_controlers.Bag_update_Page);
+router.get('/update',  secured, Bag_controlers.Bag_update_Page);
 /* GET delete bag page */
-router.get('/delete', Bag_controlers.Bag_delete_Page);
+router.get('/delete', secured, Bag_controlers.Bag_delete_Page);
 
 module.exports = router;
 
